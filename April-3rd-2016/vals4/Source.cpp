5f9b9497e18444566e80fc926d75a2ad8b49bddc
CptDemocracy <profit234@gmail.com>
2016-04-03 18:42:39 +0300

    Update Source.cpp

--- April-3rd-2016/vals4/Source.cpp
+++ April-3rd-2016/vals4/Source.cpp
@@ -1,10 +1,8 @@
 /* "Programming Principles" by B.Stroustrup
-    p.85 Exercise 6
-    Write a program that prompts the user to enter three integer values, and
-    then outputs the values in numerical sequence separated by commas. So,
-    if the user enters the values 10 4 6, the output should be 4, 6, 10. If two
-    values are the same, they should just be ordered together. So, the input 4
-    54 should give 4, 4, 5.
+    p.85 Exercise 7
+    Do exercise 6 (p.85), but with three string values. So, if the user 
+    enters the values "Steinbeck", "Hemingway", "Fitzgerald", the output 
+    should be "Fitzgerald, Hemingway, Steinbeck".
 */
 
 #include <algorithm>
