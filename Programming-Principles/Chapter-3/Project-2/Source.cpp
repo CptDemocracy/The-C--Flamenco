ccaee3eca4f19199484529ab389835cbbbd2275b
Efim Bogdanovsky <profit234@gmail.com>
2016-05-21 02:21:22 +0300

    Create Source.cpp

---
+++ Programming-Principles/Chapter-3/Project-2/Source.cpp
@@ -0,0 +1,58 @@
+/*
+ * <book>
+ *   <author>   Bjarne Stroustrup       </author>
+ *   <name>     Programming Principles  </name>
+ *   <chapter>  3                       </chapter>
+ *   <page>     85                      </page>
+ * </book>
+
+ * <exercise>   2                       </exercise>
+ *
+ * <summary>
+ *   Write a program in C++ that converts from miles to kilometers. Your
+ *   program should have a reasonable prompt for the user to enter a number
+ *   of miles. Hint: There are 1.609 kilometers to the mile.
+ *  </summary>
+ *
+ * <author> Efim Bogdanovsky    </author>
+ * <date>   May 21st 2016       </date>
+ */
+
+#include <iostream>
+
+namespace {
+    double MilesToKilometers(double miles);
+}
+
+int main(const int argc, const char *argv[]) 
+{
+    double miles { 0.0 };
+    double km    { 0.0 };
+
+    while (true)
+    {
+        static_cast<void>(std::cout << "Please enter the number of miles to convert to kilometers: ");
+
+        while ( !(std::cin >> miles)) {
+            static_cast<void>(std::cin.clear());
+            static_cast<void>(std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'));
+            static_cast<void>(std::cerr << "Input invalid. Please try again." << std::endl);
+        }
+        static_cast<void>(std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'));
+    
+        km = MilesToKilometers(miles);
+    
+        static_cast<void>(std::cout << "There are " << km << " kilometers in " 
+                                    << miles << " mile" << (miles == 1 ? "" : "s") << '.' << std::endl);
+        static_cast<void>(std::cout << "Please press Enter to continue...");
+        static_cast<void>(std::cin.get());
+    }
+	  return EXIT_SUCCESS;
+}
+
+namespace {
+    double MilesToKilometers(double miles) {
+        constexpr static double KM_IN_MI { 1.609 };
+        return (miles * KM_IN_MI);
+    }
+}
